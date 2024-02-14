b7c9b695a0dc4dab21c85ec980440fea434fbf71
Boskez97 <157408712+Boskez97@users.noreply.github.com>
2024-02-14 09:37:25 -0700

    Update casa_8.py

--- casa_8.py
+++ casa_8.py
@@ -1,1 +1,11 @@
+import pandas as pd
 
+data = {'name': ['Mexico','Poland'],
+        'currency_code': ['MXN', 'PLN']}
+
+df = pd.read_csv('big-mac-full-index.csv')
+#iterrows method:
+for i,r in df.iterrows():
+    print(r['name'], r['currency_code'])
+#apply method:
+print(df.apply(lambda row: row["name"] + ' ' + str(row['currency_code']), axis =1))
